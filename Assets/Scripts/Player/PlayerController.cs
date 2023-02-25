f26970c5118977e7c87e78c2eb737b0f426a31f2
MWA <kevin.zenou@live.fr>
2023-02-25 17:29:47 -0500

    modifs PlayerInput
    prends en compte zqsd/wasd pour le mouvement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,45 +5,96 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
     [SerializeField] private float movementSpeed;
-    private Vector2 moveInputValue;
-    float AxisX, AxisY;
 
-    // Start is called before the first frame update
+    private Vector2 move; //
+
+
+    //points d'accroche des attaques
+    public Transform attackPointUp;
+    public Transform attackPointUpRight;
+    public Transform attackPointRight;
+    public Transform attackPointDownRight;
+    public Transform attackPointDown;
+    public Transform attackPointDownLeft;
+    public Transform attackPointLeft;
+    public Transform attackPointUpLeft;
+
+    private float attackRadius = 2.0f; //rayon dans lequel la balle peut être frapper
+    public LayerMask ballLayer;
+
+
     private void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        Moving();
+        rb.MovePosition(rb.position + move * Time.fixedDeltaTime * movementSpeed); //moves according to OnMove actions
     }
 
-    private void Moving()
+    private void OnMove(InputValue val)  //Listening movement inputs (zqsd/wasd)
     {
-        transform.Translate(moveInputValue * movementSpeed * Time.deltaTime);
+        move = val.Get<Vector2>();
     }
 
-    private void OnMoveHorizontal(InputValue  val)
+
+
+   /* private void OnMoveStick(InputValue val)   //à tester avec une manette si ça fonctionne sans ça alors ça dégage <3
     {
-        AxisX = val.Get<float>();
-        moveInputValue = new Vector2(AxisX, AxisY);
-        moveInputValue.Normalize();
-    }
+        move = val.Get<Vector2>();
+    }*/
 
 
-    private void OnMoveVertical(InputValue val)
+
+    /*void Hit()
     {
-        AxisY = val.Get<float>();
-        moveInputValue = new Vector2(AxisX, AxisY);
-        moveInputValue.Normalize();
-    }
+        Collider2D[] hit;
+        if (animator.GetFloat("Vertical") >= 0 && animator.GetFloat("Horizontal") == 0) // 12h
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointUp.position, attackRadius, ballLayer);
+        }
+       else if (animator.GetFloat("Vertical") >= 0 && animator.GetFloat("Horizontal") >= 0) // 1h30
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointUpRight.position, attackRadius, ballLayer);
+        }
+       else if (animator.GetFloat("Vertical") == 0 && animator.GetFloat("Horizontal") >= 0) // 3h
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointRight.position, attackRadius, ballLayer);
+        }
+        else if (animator.GetFloat("Vertical") <= 0 && animator.GetFloat("Horizontal") >= 0) // 4h30
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointDownRight.position, attackRadius, ballLayer);
+        }
+        else if (animator.GetFloat("Vertical") <= 0 && animator.GetFloat("Horizontal") == 0) // 6h
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointDown.position, attackRadius, ballLayer);
+        }
+        else if (animator.GetFloat("Vertical") <= 0 && animator.GetFloat("Horizontal") <= 0) // 7h30
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointDownLeft.position, attackRadius, ballLayer);
+        }
+        else if (animator.GetFloat("Vertical") == 0 && animator.GetFloat("Horizontal") <= 0) // 9h
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointLeft.position, attackRadius, ballLayer);
+        }
+        else if (animator.GetFloat("Vertical") >= 0 && animator.GetFloat("Horizontal") <= 0) // 10h30
+        {
+            hit = Physics2D.OverlapCircleAll(attackPointUpLeft.position, attackRadius, ballLayer);
+        }
 
-    private void OnMoveStick(InputValue val)
-    {
-        moveInputValue = val.Get<Vector2>();
-        Debug.Log(moveInputValue);
-    }
+        foreach (Collider2D ball in hit)
+        {
+            if(ball.gameObject.tag == "Ball")
+            {
+                Debug.Log("ça marche" + ball.gameObject.name);
+            }
+        }
+
+        
+    }*/
 
 }
+
