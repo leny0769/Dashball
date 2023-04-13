256cf5dd1bbfc8929be65e2bb3151e99d762d3ba
leny0769 <leny-07@hotmail.fr>
2023-04-13 15:26:17 -0400

    Amélioration Main Menu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 
 public class MainMenu : MonoBehaviour
 {
     public string sceneToLoad;
     public GameObject settingsWindow;
+    public Button settingsButton;
+    public Button playButton;
+    public Button quitButton;
+
+
+    void Update()
+    {
+        if (settingsWindow.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+           CloseSettingsButton();
+        }
+    }
 
     public void StartGame()
     {
@@ -17,11 +31,18 @@ public class MainMenu : MonoBehaviour
     public void SettingsButton()
     {
         settingsWindow.SetActive(true);
+        playButton.interactable = false;
+        quitButton.interactable = false;
+        settingsButton.interactable = false;
     }
 
     public void CloseSettingsButton()
     {
         settingsWindow.SetActive(false);
+        playButton.interactable = true;
+        quitButton.interactable = true;
+        settingsButton.interactable = true;
+        settingsButton.Select();
     }
 
     public void QuitGame()
