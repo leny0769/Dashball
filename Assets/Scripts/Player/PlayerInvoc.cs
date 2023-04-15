6749c0a44bfe6ec649446ccb12de62bcefcad974
Noecv <76057889+Noecv@users.noreply.github.com>
2023-04-15 21:15:15 +0200

    quasi fini

--- Assets/Scripts/Player/PlayerInvoc.cs
+++ Assets/Scripts/Player/PlayerInvoc.cs
@@ -7,14 +7,22 @@ public class PlayerInvoc : MonoBehaviour
 
     public PlayerConfig playerConfig {get; set;}
     [SerializeField]
+    private Animator animator;
+    [SerializeField]
     private SpriteRenderer artworkSprite;
     public CharacterDatabase characterDB;
 
+
+    void Start()
+    {
+        artworkSprite = gameObject.GetComponent<SpriteRenderer>();
+    }
 
     public void PlayerInit(PlayerConfig pc)
     {
     playerConfig = pc;
     Character character = characterDB.GetCharacter(playerConfig.character);
     artworkSprite.sprite = character.characterSprite;
+    animator.runtimeAnimatorController = character.characterAnimator;
     }
 }
